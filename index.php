aa67f90827f20f85b1023c4ddd91e9825f7d0512
Gabrielb-Webdev <Gabrielbwebdev@gmail.com>
2024-09-20 09:52:59 -0300

    Version Mobile Home

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/resposive.css">
 </head>
 
 <body>
-
     <!-- Start of Navbar -->
     <?php include 'components/navbar.php'; ?>
     <!-- End of Navbar -->
